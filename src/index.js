902dda3624af60000292e76157859449bbb6e6a0
shayxu <shayxu@tencent.com>
2021-06-14 16:10:34 +0800

    fix: 适配vue3，fixes#6

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import vuePrint from './print'
 const install = (Vue) => {
-    //vue2
-    if (typeof Vue === 'function') {
-        Vue.prototype.$p = vuePrint
-    //vue3
-    } else {
-        Vue.config.globalProperties.$p = vuePrint
-    }
+    Vue.prototype.$p = vuePrint
 }
 export default install
